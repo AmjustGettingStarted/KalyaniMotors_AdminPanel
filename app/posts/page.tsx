3f9bca4fca4393eff679369e6e58377790403281
AmJustGettingStarted <gamerhmv2050@gmail.com>
2025-05-30 16:55:10 +0530

    Posts page basic setup started

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -1,7 +1,23 @@
+import { Shield, Sidebar } from "lucide-react";
 import React from "react";
 
 const Posts = () => {
-  return <div>Posts</div>;
+  return (
+    <div className="px-6 pt-8">
+      {/* Top Header */}
+      <div className="border-b border-gray-300 pb-3 flex">
+        <div className=" p-4 ">
+          <Sidebar size={15} />
+        </div>
+        <div className="border-l border-gray-300 flex items-center">
+          <div className="flex items-center justify-center w-6 h-6 bg-blue-600 mx-2 ">
+            <Shield size={13} color="#fff" />
+          </div>
+          <p>Posts & Comments Management</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Posts;

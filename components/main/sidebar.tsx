b1e9fecea4e9ce2de41fd49ec26b0adf90a9fc46
AmJustGettingStarted <gamerhmv2050@gmail.com>
2025-05-30 16:54:37 +0530

    SideBar rough setup completed 😊

--- components/main/sidebar.tsx
+++ components/main/sidebar.tsx
@@ -12,46 +12,46 @@ export const sidebar_routes = [
 const Sidebar = () => {
   return (
     <div className=" w-71 h-full bg-[#FAFAFA] fixed left-0 top-0 border-r border-gray-300">
-      <div className="px-4 py-4 h-full">
+      <div className="h-full">
         <div className="flex flex-col justify-between h-full ">
+          {/* Top Content */}
           <div className="flex flex-col">
-            
-            {/* Top Content */}
-            <div className="flex gap-2">
-              <div className="flex items-center w-8 h-8 bg-blue-600    ">
-                <Shield
-                  size={18}
-                  color="#fff"
-                  className="flex items-center justify-center"
-                />
+            {/* Top First */}
+            <div className="flex items-center gap-2 pb-6 border-b border-gray-300 px-4 py-4">
+              <div className="flex items-center justify-center w-9 h-9 bg-blue-600 rounded-md ">
+                <Shield size={18} color="#fff" className="" />
               </div>
               <div>
                 <p className="text-xl font-bold">Admin Panel</p>
-                <p className="text-sm text-gray-500">Content Moderation</p>
+                <p className=" text-gray-500 tracking-tight">
+                  Content Moderation
+                </p>
               </div>
             </div>
 
             {/* Top Second */}
-            <div>
+            <div className="px-4 py-2 flex flex-col  space-y-2">
               {sidebar_routes.map((items, i) => (
                 <Link href={items.href} key={i}>
-                  <p className="flex ">
-                    <items.icon className="mr-2" />
-                    {items.label}
-                  </p>
+                  <div className="px-2 py-1 hover:bg-[#F4F4F5] hover:text-[#18181b] hover:font-semibold">
+                    <p className="flex items-center text-[#3f3f46] ">
+                      <items.icon className="mr-4" size={18} />
+                      {items.label}
+                    </p>
+                  </div>
                 </Link>
               ))}
             </div>
           </div>
 
           {/* Bottom Content */}
-          <div className="flex flex-col">
-            <p>Logged in as Admin</p>
-            <p>
-              <Button variant="ghost" className="flex">
-                <LogOut className="mr-2" /> Sign out
+          <div className="fex flex-col border-t border-gray-300">
+            <div className="p-4">
+              <p className=" text-[#475569] pb-2">Logged in as Admin</p>
+              <Button variant="outline" className="w-full justify-start cursor-pointer bg-white border-gray-200 border py-4 text-base text-[#0f172a] font-medium ">
+                <LogOut className="mr-2" size={20} /> Sign Out
               </Button>
-            </p>
+            </div>
           </div>
         </div>
       </div>

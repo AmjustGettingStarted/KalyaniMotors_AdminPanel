682ed14320de02fdbd213b596240cf64105b0c3a
AmJustGettingStarted <gamerhmv2050@gmail.com>
2025-05-31 10:24:57 +0530

    Added a function which fetches the css for the card based on the posts.data heading

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -1,31 +1,29 @@
 import { Card, CardContent } from "@/components/ui/card";
-import { CheckCircle, Clock, FilesIcon, Shield, Sidebar } from "lucide-react";
+import { Shield, Sidebar } from "lucide-react";
 import React from "react";
+import { posts_data } from "@/data/posts";
 
-export const posts_data = [
-  {
-    heading: "Total Posts",
-    icon: FilesIcon,
-    count: 20,
-    subhead: "All content",
-    css: "border-2 border-slate-200 hover:border-slate-300 bg-[#FAFCFD] text-slate-600",
-  },
-  {
-    heading: "Pending Posts",
-    icon: Clock,
-    count: 6,
-    subhead: "Awaiting review",
-    css: "border-2 border-amber-200 hover:border-amber-300 bg-gradient-to-br from-amber-50 to-orange-50 text-amber-700 ",
-  },
-  {
-    heading: "Approved Posts",
-    icon: CheckCircle,
-    count: 7,
-    subhead: "Published content",
-    css: "border-2 border-green-200 hover:border-green-300 bg-gradient-to-br from-green-50 to-emerald-50 text-green-700",
-  },
-];
 const Posts = () => {
+  // Function to determine the css of the posts data
+  function getCss(heading: string): string {
+    switch (heading.toLowerCase()) {
+      case "total posts":
+        return "border-2 border-slate-200 hover:border-slate-300 bg-[#FAFCFD] text-slate-600";
+      case "pending posts":
+        return "border-2 border-amber-200 hover:border-amber-300 bg-gradient-to-br from-amber-50 to-orange-50 text-amber-700 ";
+      case "approved posts":
+        return "border-2 border-green-200 hover:border-green-300 bg-gradient-to-br from-green-50 to-emerald-50 text-green-700";
+      case "rejected posts":
+        return "border-2 border-red-200 hover:border-red-300 bg-gradient-to-br from-red-50 to-rose-50 text-red-700";
+      case "total comments":
+        return "border-2 border-blue-200 hover:border-blue-300 bg-gradient-to-br from-blue-50 to-indigo-50 text-blue-700";
+      case "engagement":
+        return "border-2 border-purple-200 hover:border-purple-300 bg-gradient-to-br from-purple-50 to-violet-50 text-purple-700";
+      default:
+        return "";
+    }
+  }
+
   return (
     <div className="px-6 pt-8">
       {/* Top Header */}
@@ -44,7 +42,7 @@ const Posts = () => {
       <div className="p-6">
         <div className="grid grid-cols-3 gap-8">
           {posts_data.map((post, i) => (
-            <Card key={i} className={post.css}>
+            <Card key={i} className={getCss(post.heading)}>
               <CardContent>
                 <div className="p-6 flex-col">
                   <div className="w-full flex justify-between items-center">

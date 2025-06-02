d32f8df9d91b83c0888b01359b8cef23e77aba6b
AmJustGettingStarted <gamerhmv2050@gmail.com>
2025-06-02 10:43:21 +0530

    style(Posts Details): Add Style to this component

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -33,17 +33,17 @@ const Posts = () => {
   function getCss(heading: string): string {
     switch (heading.toLowerCase()) {
       case "total posts":
-        return "border-2 border-slate-200 hover:border-slate-300 bg-[#FAFCFD] text-slate-600";
+        return "border-2 round-sm border-slate-200 hover:border-slate-300 bg-[#FAFCFD] text-slate-600";
       case "pending posts":
-        return "border-2 border-amber-200 hover:border-amber-300 bg-gradient-to-br from-amber-50 to-orange-50 text-amber-700 ";
+        return "border-2 round-sm border-amber-200 hover:border-amber-300 bg-gradient-to-br from-amber-50 to-orange-50 text-amber-700 ";
       case "approved posts":
-        return "border-2 border-green-200 hover:border-green-300 bg-gradient-to-br from-green-50 to-emerald-50 text-green-700";
+        return "border-2 round-sm border-green-200 hover:border-green-300 bg-gradient-to-br from-green-50 to-emerald-50 text-green-700";
       case "rejected posts":
-        return "border-2 border-red-200 hover:border-red-300 bg-gradient-to-br from-red-50 to-rose-50 text-red-700";
+        return "border-2 round-sm border-red-200 hover:border-red-300 bg-gradient-to-br from-red-50 to-rose-50 text-red-700";
       case "total comments":
-        return "border-2 border-blue-200 hover:border-blue-300 bg-gradient-to-br from-blue-50 to-indigo-50 text-blue-700";
+        return "border-2 round-sm border-blue-200 hover:border-blue-300 bg-gradient-to-br from-blue-50 to-indigo-50 text-blue-700";
       case "engagement":
-        return "border-2 border-purple-200 hover:border-purple-300 bg-gradient-to-br from-purple-50 to-violet-50 text-purple-700";
+        return "border-2 round-sm border-purple-200 hover:border-purple-300 bg-gradient-to-br from-purple-50 to-violet-50 text-purple-700";
       default:
         return "";
     }
@@ -55,9 +55,9 @@ const Posts = () => {
   // const repeatedPosts = Array.from({ length: 5 }, () => allPostsData).flat();
 
   return (
-    <div className="p-6">
+    <div className="p-3 md:p-6">
       {/* Top Header */}
-      <div className="border-b border-gray-300 p-4 flex">
+      <div className="border-b border-gray-300 p-2 md:p-4 flex">
         <div className="">
           <Sidebar size={20} className="m-2" />
         </div>
@@ -71,19 +71,19 @@ const Posts = () => {
       </div>
       {/* Posts Details Cards */}
       <div className="py-6">
-        <div className="grid grid-cols-3 gap-8">
+        <div className="grid grid-cols-2 md:grid-cols-3 gap-8">
           {posts_data.map((post, i) => (
             <Card key={i} className={getCss(post.heading)}>
               <CardContent>
                 <div className=" flex-col">
                   <div className="w-full flex justify-between items-center">
-                    <p>{post.heading}</p>
+                    <p className="font-semibold">{post.heading}</p>
                     <p>
-                      <post.icon />
+                      <post.icon size={18} />
                     </p>
                   </div>
-                  <p>{post.count}</p>
-                  <p>{post.subhead}</p>
+                  <p className="text-2xl font-bold mt-2">{post.count}</p>
+                  <p className="text-xs my-2">{post.subhead}</p>
                 </div>
               </CardContent>
             </Card>
--- data/posts.ts
+++ data/posts.ts
@@ -2,7 +2,7 @@ import {
   CheckCircle,
   CircleX,
   Clock,
-  FilesIcon,
+  FileSpreadsheet,
   MessageSquare,
   Sparkles,
 } from "lucide-react";
@@ -10,45 +10,45 @@ import {
 export const posts_data = [
   {
     heading: "Total Posts",
-    icon: FilesIcon,
+    icon: FileSpreadsheet,
     count: 20,
     subhead: "All content",
-    // css: "border-2 border-slate-200 hover:border-slate-300 bg-[#FAFCFD] text-slate-600",
+    // css: "border-2 round-sm border-slate-200 hover:border-slate-300 bg-[#FAFCFD] text-slate-600",
   },
   {
     heading: "Pending Posts",
     icon: Clock,
     count: 6,
     subhead: "Awaiting review",
-    // css: "border-2 border-amber-200 hover:border-amber-300 bg-gradient-to-br from-amber-50 to-orange-50 text-amber-700 ",
+    // css: "border-2 round-sm border-amber-200 hover:border-amber-300 bg-gradient-to-br from-amber-50 to-orange-50 text-amber-700 ",
   },
   {
     heading: "Approved Posts",
     icon: CheckCircle,
     count: 7,
     subhead: "Published content",
-    // css: "border-2 border-green-200 hover:border-green-300 bg-gradient-to-br from-green-50 to-emerald-50 text-green-700",
+    // css: "border-2 round-sm border-green-200 hover:border-green-300 bg-gradient-to-br from-green-50 to-emerald-50 text-green-700",
   },
   {
     heading: "Rejected Posts",
     icon: CircleX,
     count: 7,
     subhead: "Declined content",
-    // css: "border-2 border-red-200 hover:border-red-300 bg-gradient-to-br from-red-50 to-rose-50 text-red-700",
+    // css: "border-2 round-sm border-red-200 hover:border-red-300 bg-gradient-to-br from-red-50 to-rose-50 text-red-700",
   },
   {
     heading: "Total Comments",
     icon: MessageSquare,
     count: 188,
     subhead: "User interactions",
-    // css: "border-2 border-blue-200 hover:border-blue-300 bg-gradient-to-br from-blue-50 to-indigo-50 text-blue-700",
+    // css: "border-2 round-sm border-blue-200 hover:border-blue-300 bg-gradient-to-br from-blue-50 to-indigo-50 text-blue-700",
   },
   {
     heading: "Engagement",
     icon: Sparkles,
     count: "",
     subhead: "Likes & shares",
-    // css: "border-2 border-purple-200 hover:border-purple-300 bg-gradient-to-br from-purple-50 to-violet-50 text-purple-700",
+    // css: "border-2 round-sm border-purple-200 hover:border-purple-300 bg-gradient-to-br from-purple-50 to-violet-50 text-purple-700",
   },
 ];
 

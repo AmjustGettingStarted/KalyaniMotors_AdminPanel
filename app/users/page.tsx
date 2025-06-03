d719faaba4c14d22ef497181273dcb0f6aec5dbf
AmJustGettingStarted <gamerhmv2050@gmail.com>
2025-06-03 10:11:37 +0530

    feat(users): Add filter & search component

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,11 +1,16 @@
+import SelectMenu from "@/components/sub/select-menu";
 import { Card, CardContent } from "@/components/ui/card";
-import { users_count } from "@/data/users";
+import { Input } from "@/components/ui/input";
+import { filterData } from "@/data/posts";
+import { users_count, users_select } from "@/data/users";
+import { ArrowDownUpIcon, Filter, Search } from "lucide-react";
 import React from "react";
 
 const Users = () => {
   return (
     <div className="p-6">
-      <div className="space-y-4 flex-col">
+      <div className="space-y-8 flex flex-col">
+        {/* Top Header */}
         <div>
           <h1 className="text-4xl tracking-tight font-semibold">
             User Management
@@ -24,7 +29,6 @@ const Users = () => {
                   <p>{item.lable}</p>
                   <p>
                     <item.icon />
-                    
                   </p>
                 </div>
                 <p>{item.count}</p>
@@ -32,6 +36,32 @@ const Users = () => {
             </Card>
           ))}
         </div>
+
+        {/* Filter component */}
+        <div className="p-0 md:p-4 md:border md:border-gray-300 rounded-md">
+          <div className="flex-col md:flex-row md:flex w-full">
+            <div className="w-full md:w-1/2 flex items-center space-x-2 ">
+              <Search size={18} className="text-slate-400" />
+              <Input
+                placeholder="Search by username, email, or user ID..."
+                className="my-2 h-10 placeholder:text-base truncate"
+              />
+            </div>
+            <div className="flex w-full md:w-1/2 md:pl-4 space-x-2">
+              <p className="flex items-center w-1/2 gap-2">
+                <Filter size={18} className="text-slate-400" />
+                <SelectMenu data={users_select} />
+              </p>
+              <p className="flex items-center w-1/2 gap-2">
+                <ArrowDownUpIcon className="text-slate-400" />
+                <SelectMenu data={filterData} />
+              </p>
+            </div>
+          </div>
+        </div>
+
+        {/* All Users  */}
+        <div className="flex flex-col space-y-4"></div>
       </div>
     </div>
   );

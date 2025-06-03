ac877e84c90a46531d69059953ce20340cfa287c
AmJustGettingStarted <gamerhmv2050@gmail.com>
2025-06-03 09:53:24 +0530

    feat(users): Add users data cards

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,3 +1,5 @@
+import { Card, CardContent } from "@/components/ui/card";
+import { users_count } from "@/data/users";
 import React from "react";
 
 const Users = () => {
@@ -14,7 +16,22 @@ const Users = () => {
         </div>
 
         {/* Users Count Card Section */}
-        <div></div>
+        <div className="grid grid-cols-2 lg:grid-cols-4 gap-4">
+          {users_count.map((item, i) => (
+            <Card key={i} className="rounded-sm">
+              <CardContent className="space-y-4">
+                <div className="flex justify-between items-center">
+                  <p>{item.lable}</p>
+                  <p>
+                    <item.icon />
+                    
+                  </p>
+                </div>
+                <p>{item.count}</p>
+              </CardContent>
+            </Card>
+          ))}
+        </div>
       </div>
     </div>
   );
--- data/users.ts
+++ data/users.ts
@@ -1,9 +1,8 @@
-import { CircleCheckBig, CircleOff, Users } from "lucide-react";
+import { Ban, CircleCheckBig, Users } from "lucide-react";
 
 export const users_count = [
   { lable: "Total Users", icon: Users, count: 20 },
   { lable: "Active Users", icon: CircleCheckBig, count: 18 },
-  { lable: "Blocked", icon: CircleOff, count: 2 },
+  { lable: "Blocked", icon: Ban, count: 2 },
   { lable: "Total Posts", icon: Users, count: 413 },
-  {},
 ];

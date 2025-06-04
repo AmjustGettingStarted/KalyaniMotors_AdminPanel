5275bffa2b22cc8f61a613ce0bbcdea54453e892
AmJustGettingStarted <gamerhmv2050@gmail.com>
2025-06-04 10:11:57 +0530

    feat(settings): add appearance & notfications card (base setup)

--- app/settings/page.tsx
+++ app/settings/page.tsx
@@ -1,3 +1,4 @@
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import React from "react";
 
 const Settings = () => {
@@ -12,6 +13,32 @@ const Settings = () => {
           <h3 className="text-sm sm:text-lg font-normal text-slate-600 pt-2">
             Configure admin panel preferences and moderation rules
           </h3>
+        </div>
+
+        {/* Appearance & Notfication */}
+        <div className="grid md:grid-cols-2 gap-6">
+          {/* Appearance */}
+          <div>
+            <Card className="rounded-sm shadow-xs">
+              <CardHeader>
+                <CardTitle>Card Title</CardTitle>
+              </CardHeader>
+              <CardContent>
+                <p>Card Content</p>
+              </CardContent>
+            </Card>
+          </div>
+          {/* Notifications */}
+          <div>
+            <Card className="rounded-sm shadow-xs">
+              <CardHeader>
+                <CardTitle>Card Title</CardTitle>
+              </CardHeader>
+              <CardContent>
+                <p>Card Content</p>
+              </CardContent>
+            </Card>
+          </div>
         </div>
       </div>
     </div>

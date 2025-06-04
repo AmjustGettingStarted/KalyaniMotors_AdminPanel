2da2a438806e83c844e7d06314cd5f50345fda05
AmJustGettingStarted <gamerhmv2050@gmail.com>
2025-06-04 10:02:03 +0530

    feat(Settings): Add header

--- app/settings/page.tsx
+++ app/settings/page.tsx
@@ -1,7 +1,21 @@
 import React from "react";
 
 const Settings = () => {
-  return <div>Settings</div>;
+  return (
+    <div className="p-6">
+      <div className="space-y-4 sm:space-y-8 flex flex-col">
+        {/* Top Header */}
+        <div>
+          <h1 className="text-xl sm:text-4xl tracking-tight font-semibold">
+            Admin Settings
+          </h1>
+          <h3 className="text-sm sm:text-lg font-normal text-slate-600 pt-2">
+            Configure admin panel preferences and moderation rules
+          </h3>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Settings;

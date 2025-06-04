7b6fd8afeeaccd3b251e2cce281e7c80b0c132f9
AmJustGettingStarted <gamerhmv2050@gmail.com>
2025-06-04 10:29:35 +0530

    feat(settings): add color data for primary colors

---
+++ data/settings.ts
@@ -0,0 +1,8 @@
+export const choose_colors = [
+  { bg: "bg-blue-500", color: "Blue" },
+  { bg: "bg-green-500", color: "Green" },
+  { bg: "bg-purple-500", color: "Purple" },
+  { bg: "bg-orange-500", color: "Orange" },
+  { bg: "bg-red-500", color: "Red" },
+  { bg: "bg-slate-500", color: "Slate" },
+];

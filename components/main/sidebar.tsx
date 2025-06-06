95c48b3aa2053b22a86011971243dd4d2bfe6427
AmJustGettingStarted <gamerhmv2050@gmail.com>
2025-06-06 15:48:10 +0530

    styles(Sidebar): Fix Sidebar styling

--- components/main/sidebar.tsx
+++ components/main/sidebar.tsx
@@ -1,10 +1,10 @@
-import { File, LogOut, Settings, Shield, Users } from "lucide-react";
+import { FileText, LogOut, Settings, Shield, Users } from "lucide-react";
 import Link from "next/link";
 import React from "react";
 import { Button } from "../ui/button";
 
 export const sidebar_routes = [
-  { label: "Posts", icon: File, href: "/posts" },
+  { label: "Posts", icon: FileText, href: "/posts" },
   { label: "Users", icon: Users, href: "/users" },
   { label: "Settings", icon: Settings, href: "/settings" },
 ];
@@ -17,13 +17,13 @@ const Sidebar = () => {
           {/* Top Content */}
           <div className="flex flex-col">
             {/* Top First */}
-            <div className="flex items-center gap-2 pb-6 border-b border-gray-300 px-4 py-4">
-              <div className="flex items-center justify-center w-9 h-9 bg-blue-600 rounded-md ">
-                <Shield size={18} color="#fff" className="" />
+            <div className="flex items-center gap-2 border-b border-gray-300 p-4">
+              <div className="flex items-center justify-center w-8 h-8 bg-blue-600 rounded-md ">
+                <Shield size={16} color="#fff" className="" />
               </div>
               <div>
-                <p className="text-xl font-bold">Admin Panel</p>
-                <p className=" text-gray-500 tracking-tight">
+                <p className="text-lg font-bold">Admin Panel</p>
+                <p className="text-sm text-gray-500 tracking-tight">
                   Content Moderation
                 </p>
               </div>
@@ -33,10 +33,10 @@ const Sidebar = () => {
             <div className="px-4 py-2 flex flex-col  space-y-2">
               {sidebar_routes.map((items, i) => (
                 <Link href={items.href} key={i}>
-                  <div className="px-2 py-1 hover:bg-[#F4F4F5] hover:text-[#18181b] hover:font-semibold">
-                    <p className="flex items-center text-[#3f3f46] ">
-                      <items.icon className="mr-4" size={18} />
-                      {items.label}
+                  <div className="p-1 hover:bg-[#F4F4F5] hover:text-[#18181b] hover:font-semibold">
+                    <p className="flex items-center text-[#3f3f46] gap-3">
+                      <items.icon size={16} />
+                      <span className="text-sm">{items.label}</span>
                     </p>
                   </div>
                 </Link>
@@ -46,10 +46,14 @@ const Sidebar = () => {
 
           {/* Bottom Content */}
           <div className="fex flex-col border-t border-gray-300">
-            <div className="p-4">
-              <p className=" text-[#475569] pb-2">Logged in as Admin</p>
-              <Button variant="outline" className="w-full justify-start cursor-pointer bg-white border-gray-200 border py-4 text-base text-[#0f172a] font-medium ">
-                <LogOut className="mr-2" size={20} /> Sign Out
+            <div className="p-4 space-y-2">
+              <p className=" text-[#475569] text-sm">Logged in as Admin</p>
+              <Button
+                variant="outline"
+                className="w-full flex items-center justify-start cursor-pointer bg-background border-gray-200   text-slate-500 hover:text-slate-900"
+              >
+                <LogOut className="mr-2" size={20} />
+                <span className="text-sm ">Sign Out</span>
               </Button>
             </div>
           </div>

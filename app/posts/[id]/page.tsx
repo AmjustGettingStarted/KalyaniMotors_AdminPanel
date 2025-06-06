2e0e413d0dd58d2898aeb14a61a8b4086f872a0a
AmJustGettingStarted <gamerhmv2050@gmail.com>
2025-06-06 17:09:03 +0530

    feat(post detail): Add footer buttons

--- app/posts/[id]/page.tsx
+++ app/posts/[id]/page.tsx
@@ -1,5 +1,18 @@
 import { Button } from "@/components/ui/button";
-import { ArrowLeft, Shield, Sidebar } from "lucide-react";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  ArrowLeft,
+  CircleCheckBig,
+  CircleX,
+  Shield,
+  Sidebar,
+} from "lucide-react";
 import Link from "next/link";
 import React from "react";
 
@@ -23,11 +36,38 @@ const PostDetail = ({ params }: { params: { id: string } }) => {
       <div className="space-y-6">
         <div className="pt-6">
           <Link href="/posts">
-          <Button className="" variant="outline">
-            <ArrowLeft />
-            Back to Posts
-          </Button>
+            <Button
+              className="rounded-sm cursor-pointer shadow-none"
+              variant="outline"
+            >
+              <ArrowLeft />
+              Back to Posts
+            </Button>
           </Link>
+        </div>
+        <div>
+          <Card className="h-screen rounded-sm">
+            <CardHeader>
+              <CardTitle>Title</CardTitle>
+            </CardHeader>
+            <CardContent>Content</CardContent>
+            <CardFooter className="grid grid-cols-1 md:grid-cols-2 gap-2">
+              <Button
+                className="w-full h-10 rounded-sm text-white hover:text-white bg-green-600 hover:bg-green-700"
+                variant={`outline`}
+              >
+                <CircleCheckBig />
+                Approve Post
+              </Button>
+              <Button
+                className="w-full h-10 rounded-sm text-white hover:text-white bg-red-600 hover:bg-red-700"
+                variant={`outline`}
+              >
+                <CircleX />
+                Reject Post
+              </Button>
+            </CardFooter>
+          </Card>
         </div>
       </div>
     </div>

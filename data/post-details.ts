9451783e83078f57a744d244b3aea44210a34152
AmjustGettingStarted <gamerhmv2050@gmail.com>
2025-06-14 11:37:18 +0530

    feat(comments): Add comment count data

---
+++ data/post-details.ts
@@ -0,0 +1,14 @@
+type comment = "Total" | "Pending" | "Approved" | "Rejected";
+export const commentCount: comment[] = [
+  "Total",
+  "Pending",
+  "Approved",
+  "Rejected",
+];
+
+export const comment_count = [
+  { lable: "Total", count: 18 },
+  { lable: "Pending", count: 10 },
+  { lable: "Approved", count: 3 },
+  { lable: "Rejected", count: 5 },
+];

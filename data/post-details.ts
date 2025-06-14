f635517307345a1eb6b4c2e65896738a53515e87
AmjustGettingStarted <gamerhmv2050@gmail.com>
2025-06-14 15:15:23 +0530

    Add all users new dar

--- data/post-details.ts
+++ data/post-details.ts
@@ -12,3 +12,49 @@ export const comment_count = [
   { lable: "Approved", count: 3 },
   { lable: "Rejected", count: 5 },
 ];
+
+export const all_users = [
+  {
+    avatar:
+      "https://images.pexels.com/photos/1004000/pexels-photo-1004000.jpeg?auto=compress&cs=tinysrgb&w=100",
+    name: "user9",
+    status: "Approved",
+    id: 9,
+    mail: "user9@example.com",
+    joined: "28 days",
+    posts: 9,
+    last_active: 4,
+  },
+  {
+    avatar:
+      "https://images.pexels.com/photos/1006000/pexels-photo-1006000.jpeg?auto=compress&cs=tinysrgb&w=100",
+    name: "user11",
+    status: "Rejected",
+    id: 11,
+    mail: "user11@example.com",
+    joined: "1 month",
+    posts: 4,
+    last_active: 6,
+  },
+  {
+    avatar:
+      "https://images.pexels.com/photos/1011000/pexels-photo-1011000.jpeg?auto=compress&cs=tinysrgb&w=100",
+    name: "user16",
+    status: "Pending",
+    id: 16,
+    mail: "user16@example.com",
+    joined: "3 months",
+    posts: 15,
+    last_active: 11,
+  },
+  {
+    avatar: "",
+    name: "jeff",
+    status: "Approved",
+    id: 1,
+    mail: "jeff@example.com",
+    joined: "1 days",
+    posts: 100,
+    last_active: 1,
+  },
+];
